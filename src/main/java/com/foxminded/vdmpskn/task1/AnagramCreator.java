25d4c518e79d1f4cb5193ddd3d837f64cda93bf2
Vadim Piskun <vadim.piskun.dp@gmail.com>
2023-05-15 18:16:00 +0000

    Task 1 - Anagrams

---
+++ src/main/java/com/foxminded/vdmpskn/task1/AnagramCreator.java
@@ -0,0 +1,21 @@
+package com.foxminded.vdmpskn.task1;
+
+import java.util.StringJoiner;
+
+import static com.foxminded.vdmpskn.task1.Anagrams.reverseWord;
+
+public class AnagramCreator {
+
+    public static String createSentenceAnagram(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            throw new NullPointerException("Input string cannot be null or empty.");
+        }
+        StringJoiner anagram = new StringJoiner(" ");
+        String [] splitSentence = str.split(" ");
+
+        for (String s : splitSentence) {
+            anagram.add(reverseWord(s));
+        }
+        return anagram.toString();
+    }
+}
